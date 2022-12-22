83ed288c6bc6994327cf894bc0301049be2f28ae
Kiran Shrestha <kdstha10@outlook.com>
2022-12-21 21:00:50 -0500

    Added Create VIew

--- base/views.py
+++ base/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+
+from django.urls import reverse_lazy
 from .models import Task
 
 
@@ -12,3 +15,9 @@ class TaskList(ListView):
 class TaskDetail(DetailView):
     model = Task
     context_object_name = 'tasks'
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy('task')
+    
